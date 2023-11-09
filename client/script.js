1dbcf70a992b77426076266f353a58b4d4d578c1
themanavsharma <sharmamanavv@gmail.com>
2023-11-09 14:46:57 -0500

    Suggested Step #7
    Added front end functionality to test back-end item #2.

--- client/script.js
+++ client/script.js
@@ -1,12 +1,31 @@
+//function to get suphero info based on superhero ID input
 async function getSuperheroInfo() {
-    const superheroId = document.getElementById('superheroId').value;
+
+    const superheroId = document.getElementById('superheroId').value; // gets superhero ID from the input field
   
     try {
-      const response = await fetch(`http://localhost:3000/api/superhero/${superheroId}`);
-      const superhero = await response.json();
+      const response = await fetch(`http://localhost:3000/api/superhero/${superheroId}`); // fetches superhero info from server based on ID
+      const superhero = await response.json(); 
   
       document.getElementById('superheroInfo').innerHTML = JSON.stringify(superhero, null, 2);
+
     } catch (error) {
-      console.error('Error fetching superhero information:', error);
+      console.log('Error getting the superhero information:', error); // logs an error if it can't get the superhero info
+    }
+  }
+
+//function to get suphero powers based on superhero ID input
+async function getSuperheroPowers() {
+
+    const superheroId = document.getElementById('superheroId').value; // gets superhero ID from the input field
+  
+    try {
+      const response = await fetch(`http://localhost:3000/api/superhero/${superheroId}/powers`); // fetches superhero info from server based on ID
+      const superhero = await response.json(); 
+  
+      document.getElementById('superheroInfo').innerHTML = JSON.stringify(superhero, null, 2);
+
+    } catch (error) {
+      console.log('Error getting the superhero information:', error); // logs an error if it can't get the superhero info
     }
   }
